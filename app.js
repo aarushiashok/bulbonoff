f3450b9f55da7c7be217b1bf6785a841cc7e7090
aarushiashok <aarushi.ashok@gmail.com>
2024-05-14 18:11:40 +0530

    Add files via upload
    initial

---
+++ app.js
@@ -0,0 +1,11 @@
+var bulb_on = document.getElementById("on");
+var bulb_off = document.getElementById("off");
+var img = document.getElementById("bulb");
+
+function on() {
+    img.src = "https://storage.needpix.com/rsynced_images/light-148483_1280.png";
+}
+
+function off() {
+    img.src = "https://cdn.pixabay.com/photo/2013/07/12/14/34/filament-148482_1280.png";
+}
